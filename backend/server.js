68a7a79b666014be3b570a609ec55b959d710491
Prachi Parihar <prachisingh30aabb@gmail.com>
2024-03-27 21:37:41 +0530

    Files Added

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,42 @@ const Port=process.env.PORT || 3000
 
 const app=express()
 
-app.get("/",(req,res)=>{
-    res.send("Server is ready")
+// app.get("/",(req,res)=>{
+//     res.send(`Server is ready in port${Port}`)
+// })
+
+app.get("/api/jokes",(req,res)=>{
+    const jokes=[
+        {
+            id:1,
+            title:"First joke",
+            content:"I told my wife she was drawing her eyebrows too high. She looked surprised."
+        },
+        {
+            id:2,
+            title:"Second joke",
+            content:"Why don't scientists trust atoms? Because they make up everything!"
+        },
+        {
+            id:3,
+            title:"Third joke",
+            content:"I'm reading a book on anti-gravity. It's impossible to put down!"
+        },
+        {
+            id:4,
+            title:"Forth joke",
+            content:"I'm on a whiskey diet. I've lost three days already."
+        },
+        
+        {
+            id:5,
+            title:"Fifth joke",
+            content:"Parallel lines have so much in common. It's a shame they'll never meet."
+        }
+        
+    ]
+
+    res.send(jokes)
 })
 
 app.listen(Port,()=>{
